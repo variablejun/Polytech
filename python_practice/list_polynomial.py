9a8ba6bedb42062e544317039f0114eb54ef0a56
variablejun <crescendo0217@gmail.com>
2022-04-13 14:57:28 +0900

    주석추가

--- python_practice/list_polynomial.py
+++ python_practice/list_polynomial.py
@@ -12,14 +12,18 @@ def printPoly(p_x) : # 계산식을 출력해 주는 함수
     
     return polyStr # 문자열반환
 
-def  calcPoly(xVal, p_x):
+def  calcPoly(xVal, p_x): # 실제로 게산해 주는 함수
     retValue = 0
-    term = len(p_x) -1
+    term = len(p_x) -1 # 지수는 0부터 시작한다 하지만 
+    #len함수로 길이를 구할 경우 0~3을 4로 반환하기 때문에 1개를 빼준다
     
     for i in range(len(p_x)):
         coef = p_x[i]
         retValue = retValue + coef * xVal ** term
-        term = term - 1
+        # 식을 출력해야 할땐 str 변수로 캐스팅 해주지만 이 함수는
+        #계산을 해야해서 캐스팅을 하지 않는다 **제곱수 연산
+
+        term = term - 1 #지수 감소
     
     return retValue
 
